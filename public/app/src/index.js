6cc15d004aff81f6b2e43a08c6656f8aea495f6c
movses <TP8147@equans.com>
2024-02-01 16:57:09 +0100

    react + router

---
+++ public/app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import X from './Game-x';
+import reportWebVitals from './reportWebVitals';
+import NavBar from './Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+      </Routes>
+      <App />
+      <X/>
+    </Router>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
