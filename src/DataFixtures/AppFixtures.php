e0114d4d1c38c78012f144eb18abb2a70f2c7e48
movses <TP8147@equans.com>
2024-02-02 09:33:12 +0100

    react+ router-v2 without conflict

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create();
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
