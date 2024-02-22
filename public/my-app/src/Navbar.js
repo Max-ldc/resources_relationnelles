0f5269abfa3a17ea4d3ffcce6dcbc9314d6daa36
movses <TP8147@equans.com>
2024-02-22 09:30:13 +0100

    navbar

--- public/my-app/src/Navbar.js
+++ public/my-app/src/Navbar.js
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
+import User from './user/User'
+import './navbar.css';
+
+function Navbar() {
+   return (
+      <div className='header'>
+            <nav>
+               <ul>
+                  <li>
+                     <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                     <Link to="/products">Products</Link>
+                  </li>
+               </ul>
+            </nav>
+         
+
+         <div className='header-utilisateur'>
+            <User />
+         </div>
 
 
-const Navbar = () => {
- return (
- <nav>
-       <ul>
-          <li>
-             <Link to="/">Home</Link>
-          </li>
-          <li>
-             <Link to="/about">About</Link>
-          </li>
-          <li>
-             <Link to="/products">Products</Link>
-          </li>
-       </ul>
- </nav>
- );
-};
+
+
+      </div>
+   );
+}
 
 export default Navbar;
