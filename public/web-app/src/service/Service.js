2aad0ed72706c751e640e8ad47ab9f3c964abd7d
movses <TP8147@equans.com>
2024-02-23 16:30:12 +0100

    fix code

--- public/web-app/src/service/Service.js
+++
@@ -1,9 +0,0 @@
-import baseDonne from './bd.json'
-
-//Как создат функции(свободные,без html) для применения на любой страницу
-export  const getAll =()=>{ return baseDonne }
-export const getOne =(id)=>{
-    return baseDonne.find(logement=>logement.id===id)
-}
-//или так
-// export {getAll,getOne}
