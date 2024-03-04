5bd8426d94b52d9122e77322e2227be805437ff2
movses <TP8147@equans.com>
2024-03-04 14:24:46 +0100

    home page

---
+++ public/web-app/src/home/HomePage.js
@@ -0,0 +1,323 @@
+import { Link } from "react-router-dom";
+import './homepage.css';
+
+
+function HomePage() {
+    
+    return (
+    
+      <div>
+           <main>
+              <section className='main-contanair'>
+                        {/* <article className='video'>
+                            <Link to={`/articles/${articleId}`}>Видео id={articleId}</Link>
+                        </article> */}
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Great Gatsby</div>
+                              <div><img className="article-img" src="/imgs/dicaprio1.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Tom Jarrie</div>
+                                <div className="like">
+                                  <div><img className="likeDislike-img"src="./like-svgrepo-com.svg"></img></div>
+                                  <div className="like-number">17</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">Killers of the Flower Moon</div>
+                              <div><img className="article-img" src="/imgs/dicaprio2.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                          <div className="like-block">
+                                <div>Tom Jarrie</div>
+                                <div className="like">
+                                  <div><img className="likeDislike-img"src="./like-svgrepo-com.svg"></img></div>
+                                  <div className="like-number">2900</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Men Who Built America: Frontiersmen</div>
+                              <div><img className="article-img" src="/imgs/dicaprio3.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Auteur: Tom</div>
+                                <div className="like">
+                                  <div className="like-number">17</div>
+                                  <div>like icon</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">Whose Vote Counts, Explained	</div>
+                              <div><img className="article-img" src="/imgs/dicaprio4.jpeg" alt="dicaprio1"/></div>
+                              {/* https://www.svgrepo.com/svg/474891/like    icons bibliotheque*/}
+                          </Link>
+
+                          <div className="like-block">
+                                <div>Tom Jarrie</div>
+                                <div className="like">
+                                  <div><img className="likeDislike-img"src="./like-svgrepo-com.svg"></img></div>
+                                  <div className="like-number">790000</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+
+
+
+              
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Great Gatsby</div>
+                              <div><img className="article-img" src="/imgs/dicaprio4.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Auteur: Tom</div>
+                                <div className="like">
+                                  <div className="like-number">17</div>
+                                  <div>like icon</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Great Gatsby</div>
+                              <div><img className="article-img" src="/imgs/dicaprio3.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Auteur: Tom</div>
+                                <div className="like">
+                                  <div className="like-number">17</div>
+                                  <div>like icon</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Great Gatsby</div>
+                              <div><img className="article-img" src="/imgs/dicaprio2.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Auteur: Tom</div>
+                                <div className="like">
+                                  <div className="like-number">17</div>
+                                  <div>like icon</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+                <article className='video'>
+                          <Link to={`/articles/1`} className="article-link">
+                              <div className="title-article">The Great Gatsby</div>
+                              <div><img className="article-img" src="/imgs/dicaprio1.jpeg" alt="dicaprio1"/></div>
+                          </Link>
+
+                              <div className="like-block">
+                                <div>Auteur: Tom</div>
+                                <div className="like">
+                                  <div className="like-number">17</div>
+                                  <div>like icon</div>
+                                </div>
+                              </div>
+
+                              <div className="relation-block">
+                                <div>Relation(accepté):</div>
+                                <div className="relation">
+                                  <div className="like-number">amis,famille</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-categoty-block">
+                                <div>Resource category:</div>
+                                <div className="resource-categoty">
+                                  <div className="like-number">loisirs</div>
+                                </div>
+                              </div>
+
+                              <div className="resource-type-block">
+                                <div>Resource type:</div>
+                                <div className="resource-type">
+                                  <div className="like-number">video</div>
+                                </div>
+                              </div>
+                </article>
+
+
+
+
+
+                </section>
+            </main>
+      </div>
+    );
+  }
+  
+  
+export default HomePage;
+
+
+
---
+++ public/web-app/src/resource/Resource.js
@@ -0,0 +1,11 @@
+const Resource = () => {
+    return (
+      <div className="block-utilisateur">
+        <h2>Page d'article</h2>
+      </div>
+    );
+  };
+  
+  
+  
+export default Resource;
