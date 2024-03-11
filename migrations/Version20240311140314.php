5367191131e835874e8fa4e90f3cb3c232b49a88
Pauline Desille <desillepauline@gmail.com>
2024-03-11 16:15:42 +0100

    clean version file

--- migrations/Version20240311140314.php
+++ migrations/Version20240311140314.php
@@ -7,19 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240311140314 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE "relation_type_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "resource_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "resource_metadata_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
@@ -46,7 +37,6 @@ final class Version20240311140314 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE "relation_type_id_seq" CASCADE');
         $this->addSql('DROP SEQUENCE "resource_id_seq" CASCADE');
