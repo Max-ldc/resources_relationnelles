09ffe3dd720eca209864f7481378f07b244c0f23
movses <TP8147@equans.com>
2024-03-26 11:31:46 +0100

    connectez-vous

--- public/web-app/src/account/Connecter.js
+++ public/web-app/src/account/Connecter.js
@@ -6,6 +6,36 @@
 
   const Connecter = () => {
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    //validation form
+    let usernameErrors = [];
+    let passwordErrors = [];
+    if (username.length < 3) {
+      usernameErrors.push('Username min 3 characters');
+    }
+    if (username.length > 16) {
+      usernameErrors.push('Username max 16 characters');
+    }
+    if ((username.match(/[^a-z]/ig) ?? []).length < 1) {
+      usernameErrors.push('Username must include at least 1 symbols.');
+    // if (username.match('/^[a-zA-Z0-9_]+$/')) {
+    //   errors.push('Password must include at least 2 upper case letters');
+    // }
+    // if ((username.match(/[A-Z]/g) ?? []).length < 2) {
+    //   errors.push('Password must include at least 2 upper case letters');
+    // }
+    }
+
+    if (password.length <= 3) {
+      passwordErrors.push('password min 3 characters');
+    }
+    if (password.length > 16) {
+      passwordErrors.push('password max 16 characters');
+    }
+
+
 
 
     return (
@@ -17,28 +47,40 @@
               <div className='text-accedez'>Accèdez à votre compte</div>
               <div className='text-star'>* champ obligatoire</div>
 
+          <Form className='connecter-block'>
 
-
-              <Form className='connecter-block'>
-              <TextField name="username" type="text" isRequired>
+              <TextField name="username" type="text"  isInvalid={usernameErrors.length > 0}
+                         value={username} onChange={setUsername} isRequired>
                   <Label>Nom d’utilisateur*</Label>
                   <Input value={username}  onChange={(e) => setUsername(e.target.value)}/>
-                  <FieldError />
+                  {username ? (
+                      <FieldError>
+                          <div><ul>{usernameErrors.map((error, i) => <li key={i}>{error}</li>)}</ul></div>
+                      </FieldError>
+                    ) : ('')
+                    }                       
               </TextField>
 
-              <TextField name="password" type="password" isRequired>
+
+              <TextField name="password" type="password" isInvalid={passwordErrors.length > 0}
+                         value={password} onChange={setPassword} isRequired>
                   <Label>Mot de passe*</Label>
-                  <Input />
-                  <FieldError />
+                  <Input  onChange={(e) => setPassword(e.target.value)} />
+                  {password ? (
+                      <FieldError>
+                          <ul>{passwordErrors.map((error, i) => <li key={i}>{error}</li>)}</ul>
+                      </FieldError>
+                        ) : ('')
+                  }                   
               </TextField>
-              
+
+
+
               <Button type="submit">Valider</Button>
           </Form>
 
+
           <Link to="/creer-compte" className='link-creer'>Créer un compte</Link>
-
-
-
       </div>
     </div>
 
--- public/web-app/src/account/CreateUser.js
+++ public/web-app/src/account/CreateUser.js
@@ -6,26 +6,62 @@ import { Link } from "react-router-dom";
 
 const CreateUser = () => {
     const [username, setUsername] = useState('');
-    const [email, setUseremail] = useState('');
+    const [email, setUserEmail] = useState('');
+    const [errorEmail, setErrorEmail] = useState(false);
+    // const [password, setPassword] = useState('');
+    const [messageReussir, setMessageReussir] = useState('');
 
-    // const [body, setBody] = useState('');
 
-    const handleSubmit = (e) => {
+    var response;
+
+    const creatUser = (e) => {
         e.preventDefault();
-
-        const newPost = { username, email };
+        const newPost = { username, email};
         const customConfig = {  headers: { 'Content-Type': 'application/ld+json' }};
 
-        axios.post('http://localhost/api/users', newPost, customConfig)
+        response = axios.post('http://localhost/api/users', newPost, customConfig)
             .then(response => {
-                console.log(`New Post Created with ID: ${response.data.id}`);
+                console.log(response);
+                setErrorEmail('');
+                setMessageReussir('Vous avez creet votre user');
             })
             .catch(error => {
-                console.error('Error creating a post:', error);
+                console.log('-----'+ error.response.data.detail)
+                setErrorEmail(error.response.data.detail);
             })
-        };
+    };
 
-    
+
+
+
+        //validation form
+        let usernameErrors = [];
+        let emailErrors = [];
+        // let passwordErrors = [];
+
+        if (username.length < 3) {
+          usernameErrors.push('Username moins de 3 caractères');
+        }
+        if (username.length > 16) {
+          usernameErrors.push('Username plus de 3 caractères');
+        }
+        if ((username.match(/^[a-zA-Z0-9_]*$/) ?? []).length < 1) {
+          usernameErrors.push('Username que lettres, chiffres et underscores');
+        }
+
+// 422 -  "message": "Le nom d'utilisateur ne peut pas contenir moins de 3 caractères.",
+// 400 - Cet email est déjà utilisé
+
+        if (email.length < 2) {
+            emailErrors.push('Email min 3 characters');
+        }
+        if ((email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i) ?? []).length < 1) {
+            emailErrors.push('Le "@" et "." requise');
+        }
+      
+
+
+
 
     return (
         <div className='connecter-container'>
@@ -36,64 +72,78 @@ const CreateUser = () => {
                 <div className='text-accedez'>Get started with an account</div>
                 <div className='text-star'>* champ obligatoire</div>
 
+                <div>
+                    { errorEmail && !response ? (<h3 className='errorMessageExist'>{errorEmail}</h3>
+                                  ) : ('')
+                    }
+                </div>
+                <div>
+                    { messageReussir && !errorEmail ? (<h3 className='messageReussir'>{messageReussir}</h3>
+                                  ) : ('')
+                    }
+                </div>
 
-                <Form className='connecter-block'>
-                <TextField name="username" type="text" isRequired>
-                    <Label>Nom d’utilisateur*</Label>
-                    <Input value={username}  onChange={(e) => setUsername(e.target.value)}/>
-                    <FieldError />
-                </TextField>
-                <TextField name="email" type="email" isRequired>
+
+        <Form className='connecter-block' onSubmit={creatUser}>
+
+                <TextField name="username" type="text"  isInvalid={usernameErrors.length > 0}
+                         value={username} onChange={setUsername} isRequired>
+                  <Label>Nom d’utilisateur*</Label>
+                  <Input onChange={(e) => setUsername(e.target.value)}/>
+                  {username ? (
+                      <FieldError>
+                          <div><ul>{usernameErrors.map((error, i) => <li key={i}>{error}</li>)}</ul></div>
+                      </FieldError>
+                    ) : ('')
+                    }                       
+              </TextField>
+
+
+                <TextField name="email" type="email" isInvalid={emailErrors.length > 0} 
+                           value={email} onChange={setUserEmail}  isRequired>
                     <Label>Email*</Label>
-                    <Input onChange={(e) => setUseremail(e.target.value)} />
-                    <FieldError />
+                    <Input onChange={(e) => setUserEmail(e.target.value)}/>
+                    {email ? (
+                      <FieldError>
+                          <div><ul>{emailErrors.map((error, i) => <li key={i}>{error}</li>)}</ul></div>
+                      </FieldError>
+                    ) : ('')
+                    }
                 </TextField>
 
-                <TextField name="password" type="password" isRequired>
-                    <Label>Mot de passe*</Label>
-                    <Input />
-                    <FieldError />
-                </TextField>
+{/* 
+                <TextField name="password" type="password" isInvalid={passwordErrors.length > 0}
+                         value={password} onChange={setPassword} isRequired>
+                  <Label>Mot de passe*</Label>
+                  <Input  onChange={(e) => setPassword(e.target.value)} />
+                  {password ? (
+                      <FieldError>
+                          <ul>{passwordErrors.map((error, i) => <li key={i}>{error}</li>)}</ul>
+                      </FieldError>
+                        ) : ('')
+                  }                   
+              </TextField>
 
-                <TextField name="password" type="password" isRequired>
+                <TextField name="password_verif" type="password" isRequired>
                     <Label>Confirmez le mot de passe*</Label>
                     <Input />
                     <FieldError />
-                </TextField>
+                </TextField> */}
                 
-                <Button type="submit" onSubmit={handleSubmit} >Valider</Button>
-            </Form>
+                <Button type="submit"  >Valider</Button>
+
+        </Form>
+
+
+
+
 
             <Link to="/connecter" className='link-creer'>Se connecter</Link>
 
         </div>
     </div>
 
-        // <div className='createuser'>
-        //     <h2>Create User</h2>
-        //         <Form  onSubmit={handleSubmit} className='connecter-block'>
-        //             <TextField name="username" type="text" isRequired>
-        //                 <Label>username</Label>
-        //                 <Input onChange={(e) => setUsername(e.target.value)}/>
-        //                 <FieldError />
-        //             </TextField>
-        //             <TextField name="email" type="email" isRequired>
-        //                 <Label>email</Label>
-        //                 <Input onChange={(e) => setUseremail(e.target.value)} />
-        //                 <FieldError />
-        //             </TextField>
-                    
-        //             <Button type="submit" >Submit</Button>
-        //         </Form>
-        // </div>
 
-
-        // <form onSubmit={handleSubmit} className='creat-user-block'>
-        //     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="user name" />
-        //     <input type="text" value={email} onChange={(e) => setUseremail(e.target.value)} placeholder="email" />
-        //     {/* <textarea value={body} onChange={(e) => setBody(e.target.value)} placeholder="Body"></textarea> */}
-        //     <button type="submit">Create User</button>
-        // </form>
     );
 };
 
