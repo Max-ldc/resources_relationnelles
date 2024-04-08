d4b7b08a069c53999bb43867aa4123648701bebf
movses <TP8147@equans.com>
2024-04-08 10:59:03 +0200

    m-footer-createuser-test

--- public/web-app/cypress/e2e/connectez-vous-tests.cy.js
+++ public/web-app/cypress/e2e/connectez-vous-tests.cy.js
@@ -5,7 +5,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.get('.icon-menu-open').click() //via menu
-    cy.contains('Connecter-vous').click()
+    cy.contains('Se connecter').click()
 
     cy.get('input[type="text"]').type('123')
     cy.get('input[type="password"]').type('pass')
