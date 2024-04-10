d263dabe87fbe1fed51d88ca8e4df350f3eaa501
movses <TP8147@equans.com>
2024-04-10 11:18:17 +0200

    test times

--- public/web-app/cypress/e2e/home-ressource-test.cy.js
+++ public/web-app/cypress/e2e/home-ressource-test.cy.js
@@ -1,6 +1,7 @@
 describe('Simule le clic sur un bouton "Voir" d’une ressource et vérifie la navigation', () => {
   it("devrait permettre accéder de la ressource", () => {
     cy.visit("http://localhost:3000/");
+    cy.wait(6000)
     cy.get("article:first").should("have.class", "video");
     cy.contains("Voir").click();
   });
